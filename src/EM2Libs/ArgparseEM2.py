370a451675e0f0ff861c21aa6478ccec7b806117
fplewniak <f.plewniak@unistra.fr>
2019-09-16 16:23:53 +0200

    NEW: GetSet argparse custom action to return a non-redundant set of elements from a list of elements and/or files containing one element per line.

--- src/EM2Libs/ArgparseEM2.py
+++ src/EM2Libs/ArgparseEM2.py
@@ -35,3 +35,28 @@ This method converts the argument values containing elements and/or files contai
                 except IOError:
                     arglist.append(v)
         return arglist
+
+
+class GetSet(argparse.Action):
+    """
+An argparse custom action to return a set from an argument containing a list of elements and/or file names. Files are
+supposed to contain one element of the list per line. There can be more than one file and the argument may take a
+combination of elements and files. In all cases, the returned set will contain all the specified elements keeping only
+one copy of each element. If you do not want to remove redundancy, then use GetList action instead.
+    """
+    def __init__(self, option_strings, dest, nargs='+', **kwargs):
+        if nargs != '+':
+            raise ValueError('GetList get_list_action requires nargs to be set to "+"')
+        super(GetSet, self).__init__(option_strings, dest, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, self.arg2set(values))
+
+    @staticmethod
+    def arg2set(values):
+        """
+This method converts the argument values containing elements and/or files containing elements into a set of elements.
+        :param values: argument values, this is supposed to be a list of arguments or None (returns an empty set)
+        :return: the set of elements or an empty set if the argument was None
+        """
+        return set(GetList.arg2list(values))
