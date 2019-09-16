0a950c1b7145f9d87ae040e5eeef64a1010d3d6a
fplewniak <f.plewniak@unistra.fr>
2019-09-16 16:32:17 +0200

    Quick fix of simple quotes changed to double quotes in docstring

--- tests/ArgparseEM2_test.py
+++ tests/ArgparseEM2_test.py
@@ -1,12 +1,12 @@
-'''
+"""
 Tests for the extension module to the standard argparse module.
-'''
+"""
 import unittest
 from EM2Libs import ArgparseEM2
 
 
 class GetListAction(unittest.TestCase):
-    # Test the GetList argparse custom get_list_action that returns a list from either a list of arguments and/or
+    # Test the GetList argparse custom GetList that returns a list from either a list of arguments and/or
     # file contents
     @staticmethod
     def test_fromfile():
@@ -33,3 +33,47 @@ class GetListAction(unittest.TestCase):
     @staticmethod
     def test_none():
         assert ArgparseEM2.GetList.arg2list(None) == []
+
+
+class GetSetAction(unittest.TestCase):
+    # Test the GetSet argparse custom GetSet action that returns a set from either a list of arguments and/or
+    # file contents
+    @staticmethod
+    def test_fromfile():
+        assert ArgparseEM2.GetSet.arg2set(['data/list_1columnA.txt']) == {'Item1', 'Item2', 'Item3'}
+
+    @staticmethod
+    def test_from2files():
+        assert ArgparseEM2.GetSet.arg2set(
+            ['data/list_1columnA.txt', 'data/list_1columnB.txt']) == {'Item1', 'Item2', 'Item3', 'Item4', 'Item5'}
+
+    @staticmethod
+    def test_from2files_redundant():
+        assert ArgparseEM2.GetSet.arg2set(
+            ['data/list_1columnA.txt', 'data/list_1columnC.txt']) == {'Item1', 'Item2', 'Item3', 'Item4', 'Item5'}
+
+    @staticmethod
+    def test_from_one():
+        assert ArgparseEM2.GetSet.arg2set(['a']) == {'a'}
+
+    @staticmethod
+    def test_fromlist():
+        assert ArgparseEM2.GetSet.arg2set(['a', 'b', 'c']) == {'a', 'b', 'c'}
+
+    @staticmethod
+    def test_fromlist_redundant():
+        assert ArgparseEM2.GetSet.arg2set(['a', 'b', 'c', 'a']) == {'a', 'b', 'c'}
+
+    @staticmethod
+    def test_fromfile_and_list():
+        assert ArgparseEM2.GetSet.arg2set(['data/list_1columnA.txt', 'a', 'b', 'c']) == {'Item1', 'Item2', 'Item3',
+                                                                                         'a', 'b', 'c'}
+
+    @staticmethod
+    def test_fromfile_and_list_redundant():
+        assert ArgparseEM2.GetSet.arg2set(['data/list_1columnA.txt', 'a', 'Item2', 'c']) == {'Item1', 'Item2', 'Item3',
+                                                                                             'a', 'c'}
+
+    @staticmethod
+    def test_none():
+        assert ArgparseEM2.GetSet.arg2set(None) == set()
