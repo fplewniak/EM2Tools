bf8664dff2f6a5d828905a7a0354fb0d0ba8889b
fplewniak <f.plewniak@unistra.fr>
2019-09-30 15:40:10 +0200

    Tests have been changed as we won't try (for the moment) to account for position fuzzyness and to replace == True and == False. Tests for errors and warnings.

--- tests/SeqFeature_test.py
+++ tests/SeqFeature_test.py
@@ -32,51 +32,57 @@ class SeqFeatureTests(unittest.TestCase):
 
     @classmethod
     def test_lies_within(cls):
-        assert cls.sprot.features[1].lies_within(5, 25) == 1.0
-        assert cls.sprot.features[1].lies_within(10, 25) == 0.0
-        assert cls.sprot.features[1].lies_within(19, 25) == 0.0
+        assert cls.sprot.features[1].lies_within(5, 25)
+        assert not cls.sprot.features[1].lies_within(10, 25)
+        assert not cls.sprot.features[1].lies_within(19, 25)
 
     @classmethod
     def test_lies_within_fuzzy(cls):
-        assert cls.sprot.features[4].lies_within(30, 42) == 1.0
-        assert cls.sprot.features[5].lies_within(0, 10) == 1.0
-        assert cls.sprot.features[5].lies_within(1, 10) == 1.0
-        assert cls.sprot.features[5].lies_within(1, 4) == 0.0
+        with pytest.warns(UserWarning):
+            cls.sprot.features[4].lies_within(30, 42)
+            cls.sprot.features[5].lies_within(0, 10)
+
+
+    @classmethod
+    def test_contains(cls):
+        assert cls.sprot.features[2].contains(20)
+        assert not cls.sprot.features[2].contains(35)
 
     @classmethod
     def test_overlaps(cls):
-        assert cls.sprot.features[2].overlaps(20) == 1.0
-        assert cls.sprot.features[2].overlaps(35) == 0.0
+        assert cls.sprot.features[2].overlaps(20, 25)
+        assert cls.sprot.features[2].overlaps(20, 40)
+        assert not cls.sprot.features[2].overlaps(2, 5)
 
     @classmethod
-    def test_overlaps_fuzzy(cls):
-        assert cls.sprot.features[4].overlaps(35) == 1.0
-        assert cls.sprot.features[5].overlaps(3) == 1.0
-        assert cls.sprot.features[4].overlaps(3) == 0.0
-        assert cls.sprot.features[5].overlaps(35) == 0.0
+    def test_contains_fuzzy(cls):
+        with pytest.warns(UserWarning):
+            cls.sprot.features[4].contains(35)
+            cls.sprot.features[5].contains(3)
 
     @classmethod
     def test_covers(cls):
-        assert cls.sprot.features[3].covers(15, 20) == 1.0
-        assert cls.sprot.features[3].covers(4, 20) == 0.0
+        assert cls.sprot.features[3].covers(15, 20)
+        assert not cls.sprot.features[3].covers(4, 20)
 
     @classmethod
     def test_covers_fuzzy(cls):
-        assert cls.sprot.features[4].covers(35, 38) == 1.0
-        assert cls.sprot.features[5].covers(3, 4) == 1.0
-        assert cls.sprot.features[4].covers(18, 40) == 0.0
-        assert cls.sprot.features[5].covers(10, 20) == 0.0
+        with pytest.warns(UserWarning):
+            cls.sprot.features[4].covers(35, 38)
+            cls.sprot.features[5].covers(3, 4)
 
     @classmethod
     def test_intersect(cls):
         assert cls.sprot.features[4].intersect(cls.sprot.features[7]).location == FeatureLocation(34, 37)
         assert cls.sprot.features[2].intersect(cls.sprot.features[3]).location == cls.sprot.features[6].location
+        assert cls.sprot.features[1].intersect(cls.sprot.features[3]).location == FeatureLocation(8, 18)
 
     @classmethod
     def test_intersect_errors(cls):
         with pytest.raises(ValueError, match=r'Undetermined .*'):
-            cls.sprot.features[0].intersect(cls.sprot.features[5])
-        # with pytest.raises(ValueError, match=r'Undetermined .*'):
-        #     cls.sprot.features[5].intersect(cls.sprot.features[0])
-        # with pytest.raises(ValueError, match=r'Undetermined .*'):
-        #     cls.sprot.features[0].intersect(SeqFeatureEM2(location=FeatureLocation(30, 37)))
+            cls.sprot.features[0].intersect(SeqFeatureEM2(location=FeatureLocation(30, 37)))
+
+    @classmethod
+    def test_intersect_fuzzy(cls):
+        with pytest.warns(UserWarning):
+            cls.sprot.features[5].intersect(cls.sprot.features[0])
