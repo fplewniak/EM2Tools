94e557ab21046a9aa33d50d934b67e8a366ad974
fplewniak <f.plewniak@unistra.fr>
2019-10-03 16:57:00 +0200

    overlap method implemented

--- src/EM2Libs/SeqRecord.py
+++ src/EM2Libs/SeqRecord.py
@@ -12,21 +12,31 @@ class SeqRecordEM2(SeqRecord):
     """
     Extension to Biopython SeqRecord class
     """
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
 
-    def overlap(self, start, end):
+    def __init__(self, seq, **kwargs):
+        super().__init__(seq, **kwargs)
+
+    def overlap(self, start, end=None, strand=0):
         """
         Retrieves features that overlap a given position range.
+        :param strand: strand specification of features to be returned. If strand is 0, then features on
+        both strands are returned
         :param start: start of range
-        :param end: end of range
+        :param end: end of range, if None, then end=start
         """
-        raise NotImplementedError
+        if end is None:
+            end = start
+        if strand == 0:
+            return [f for f in self.features if f.overlaps(start, end)]
+        return [f for f in self.features if f.overlaps(start, end) and f.strand == strand]
 
     def feature_after(self, position, strand=1, nearest=False):
         """
         Retrieves the features immediately after (but not overlaping) the specified position, on one strand or both.
         If nearest is True, then only the nearest ones are returned.
+        :param nearest:
+        :param strand: strand specification of features to be returned. If strand is 0, then features on
+         both strands are returned
         :param position: the position
         """
         raise NotImplementedError
@@ -35,6 +45,9 @@ class SeqRecordEM2(SeqRecord):
         """
         Retrieves the features immediately before (but not overlaping) the specified position, on one strand or both.
         If nearest is True, then only the nearest ones are returned.
+        :param nearest: if True, only the nearest features are returned. This only makes sense when strand is O
+        :param strand: strand specification of features to be returned. If strand is 0, then features on
+         both strands are returned
         :param position: the position
         """
         raise NotImplementedError
@@ -44,6 +57,9 @@ class SeqRecordEM2(SeqRecord):
         Retrieves all the features around a given position but not overlaping it. If nearest is True, then only the
         nearest features are returned.
         :param position: the position
+        :param nearest: if True, only the nearest features are returned. This only makes sense when strand is O
+        :param strand: strand specification of features to be returned. If strand is 0, then features on
+         both strands are returned
         """
         raise NotImplementedError
 
