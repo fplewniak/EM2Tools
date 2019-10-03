db04a3324eec33cd646f86ad20047f2c4702c792
fplewniak <f.plewniak@unistra.fr>
2019-10-03 15:02:14 +0200

    Test script for SeqRecordEM2 class

---
+++ tests/SeqRecord_test.py
@@ -0,0 +1,20 @@
+#  CeCILL FREE SOFTWARE LICENSE AGREEMENT Version 2.1 dated 2013-06-21
+#  Frédéric PLEWNIAK, CNRS/Université de Strasbourg UMR7156 - GMGM
+#
+
+import unittest
+
+import pytest
+
+
+class SeqRecord_FeatureTests(unittest.TestCase):
+    def test_overlap(self):
+        assert True is False
+
+    def test_nearest_feature(self):
+        assert True is False
+
+    
+class SeqRecord_StitchingTests(unittest.TestCase):
+    def test_stitch(self):
+        assert True is False
