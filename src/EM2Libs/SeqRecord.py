f081dd6c9f5d1e42c86eebae756b9bd25cbc90d7
fplewniak <f.plewniak@unistra.fr>
2019-10-04 16:44:42 +0200

    Implemented comparison methods to enable comparison of records based upon their id. Implementation of feature_after and feature_before methods surrounding_features does not work yet for nearest=True

--- src/EM2Libs/SeqRecord.py
+++ src/EM2Libs/SeqRecord.py
@@ -16,9 +16,28 @@ class SeqRecordEM2(SeqRecord):
     def __init__(self, seq, **kwargs):
         super().__init__(seq, **kwargs)
 
+    def __lt__(self, other):
+        return self.id.__lt__(other.id)
+
+    def __le__(self, other):
+        return self.id.__le__(other.id)
+
+    def __eq__(self, other):
+        return self.id.__eq__(other.id)
+
+    def __ne__(self, other):
+        return self.id.__ne__(other.id)
+
+    def __gt__(self, other):
+        return self.id.__gt__(other.id)
+
+    def __ge__(self, other):
+        return self.id.__ge__(other.id)
+
     def overlap(self, start, end=None, strand=0):
         """
         Retrieves features that overlap a given position range.
+        :return: a list of overlaping features
         :param strand: strand specification of features to be returned. If strand is 0, then features on
         both strands are returned. If feature.strand is 0, then all strands will match.
         :param start: start of range
@@ -28,18 +47,36 @@ class SeqRecordEM2(SeqRecord):
             end = start
         return [f for f in self.features if f.overlaps(start, end) and f.strand * strand >= 0]
 
-    def feature_after(self, position, strand=1, nearest=False):
+    def feature_after(self, position, strand=0, nearest=False):
         """
         Retrieves the features immediately after (but not overlaping) the specified position, on one strand or both.
         If nearest is True, then only the nearest ones are returned.
-        :param nearest:
+        :param nearest: if True, only the nearest features are returned. This only makes sense when strand is O
         :param strand: strand specification of features to be returned. If strand is 0, then features on
          both strands are returned
         :param position: the position
         """
-        raise NotImplementedError
+        after_fwd = {f: f.location.start for f in self.features if f.location.start > position and f.strand in [0, 1]}
+        after_rev = {f: f.location.end for f in self.features if f.location.end < position and f.strand in [0, -1]}
+        fwd_list = [k for k, v in after_fwd.items() if v == min(after_fwd.values())]
+        rev_list = [k for k, v in after_rev.items() if v == max(after_rev.values())]
+        if strand == 1:
+            return fwd_list
+        if strand == -1:
+            return rev_list
+        if nearest is True:
+            if len(after_fwd) == len(after_rev) == 0:
+                return []
+            if len(after_fwd) == 0:
+                return rev_list
+            if len(after_rev) == 0:
+                return fwd_list
+            if fwd_list[0].location.start - position < position - rev_list[0].location.end:
+                return fwd_list
+            return rev_list
+        return list(set(fwd_list + rev_list))
 
-    def feature_before(self, position, strand=1, nearest=False):
+    def feature_before(self, position, strand=0, nearest=False):
         """
         Retrieves the features immediately before (but not overlaping) the specified position, on one strand or both.
         If nearest is True, then only the nearest ones are returned.
@@ -48,18 +85,38 @@ class SeqRecordEM2(SeqRecord):
          both strands are returned
         :param position: the position
         """
-        raise NotImplementedError
+        before_fwd = {f: f.location.end for f in self.features if f.location.end < position and f.strand in [0, 1]}
+        before_rev = {f: f.location.start for f in self.features if f.location.start > position and f.strand in [0, -1]}
+        fwd_list = [k for k, v in before_fwd.items() if v == max(before_fwd.values())]
+        rev_list = [k for k, v in before_rev.items() if v == min(before_rev.values())]
+        if strand == 1:
+            return fwd_list
+        if strand == -1:
+            return rev_list
+        if nearest is True:
+            if len(before_fwd) == len(before_rev) == 0:
+                return []
+            if len(before_fwd) == 0:
+                return rev_list
+            if len(before_rev) == 0:
+                return fwd_list
+            if position - fwd_list[0].location.end < rev_list[0].location.start - position:
+                return fwd_list
+            return rev_list
+        return list(set(fwd_list + rev_list))
 
     def surrounding_features(self, position, strand=0, nearest=False):
         """
         Retrieves all the features around a given position but not overlaping it. If nearest is True, then only the
         nearest features are returned.
         :param position: the position
-        :param nearest: if True, only the nearest features are returned. This only makes sense when strand is O
+        :param nearest: if True, only the nearest features are returned.
         :param strand: strand specification of features to be returned. If strand is 0, then features on
          both strands are returned
         """
-        raise NotImplementedError
+        if nearest is False:
+            return list(set(self.feature_after(position, strand) + self.feature_before(position, strand)))
+
 
     def stitch(self, other=None, offset=0):
         """
--- tests/SeqRecord_test.py
+++ tests/SeqRecord_test.py
@@ -12,7 +12,7 @@ from EM2Libs.SeqFeature import SeqFeatureEM2
 from EM2Libs.SeqRecord import SeqRecordEM2
 
 
-class SeqRecord_FeatureTests(unittest.TestCase):
+class SeqRecordFeatureTests(unittest.TestCase):
     rec: SeqRecordEM2 = SeqRecordEM2(SeqEM2.dna('ATGAGTCGGTAACGATGCATGCATGCAGCTGACGC'), id='Rec', name='DummyDNA')
 
     rec.features = [
@@ -27,19 +27,78 @@ class SeqRecord_FeatureTests(unittest.TestCase):
 
     @classmethod
     def test_overlap(cls):
-        assert [f.id for f in cls.rec.overlap(16, 25)] == ['C', 'D', 'G', 'H']
-        assert [f.id for f in cls.rec.overlap(16)] == ['C', 'H']
-        assert [f.id for f in cls.rec.overlap(16, 25, strand=-1)] == ['G', 'H']
+        assert sorted([f.id for f in cls.rec.overlap(16, 25)]) == ['C', 'D', 'G', 'H']
+        assert sorted([f.id for f in cls.rec.overlap(16)]) == ['C', 'H']
+        assert sorted([f.id for f in cls.rec.overlap(16, 25, strand=-1)]) == ['G', 'H']
+        assert sorted([f.id for f in cls.rec.overlap(12, 14, strand=-1)]) == []
 
-    def test_features_after(self):
-        assert True is False
+    @classmethod
+    def test_features_after(cls):
+        assert sorted([f.id for f in cls.rec.feature_after(16)]) == ['D', 'F']
+        assert sorted([f.id for f in cls.rec.feature_after(16, nearest=True)]) == ['D']
+        assert sorted([f.id for f in cls.rec.feature_after(16, strand=1)]) == ['D']
+        assert sorted([f.id for f in cls.rec.feature_after(16, strand=-1)]) == ['F']
+        assert sorted([f.id for f in cls.rec.feature_after(14)]) == ['C', 'F']
+        assert sorted([f.id for f in cls.rec.feature_after(14, nearest=True)]) == ['C']
+        assert sorted([f.id for f in cls.rec.feature_after(14, strand=1)]) == ['C']
+        assert sorted([f.id for f in cls.rec.feature_after(14, strand=-1)]) == ['F']
+        assert sorted([f.id for f in cls.rec.feature_after(15)]) == ['F', 'H']
+        assert sorted([f.id for f in cls.rec.feature_after(15, nearest=True)]) == ['H']
+        assert sorted([f.id for f in cls.rec.feature_after(15, strand=1)]) == ['H']
+        assert sorted([f.id for f in cls.rec.feature_after(15, strand=-1)]) == ['F']
+        assert sorted([f.id for f in cls.rec.feature_after(4, strand=-1)]) == []
 
-    def test_features_before(self):
-        assert True is False
+    @classmethod
+    def test_features_before(cls):
+        assert sorted([f.id for f in cls.rec.feature_before(16)]) == ['B', 'G']
+        assert sorted([f.id for f in cls.rec.feature_before(16, nearest=True)]) == ['G']
+        assert sorted([f.id for f in cls.rec.feature_before(16, strand=1)]) == ['B']
+        assert sorted([f.id for f in cls.rec.feature_before(16, strand=-1)]) == ['G']
+        assert sorted([f.id for f in cls.rec.feature_before(20)]) == ['H']
+        assert sorted([f.id for f in cls.rec.feature_before(20, strand=1)]) == ['H']
+        assert sorted([f.id for f in cls.rec.feature_before(20, strand=-1)]) == []
 
-    def test_surrounding(self):
-        assert True is False
+    @classmethod
+    def test_surrounding(cls):
+        assert sorted([f.id for f in cls.rec.surrounding_features(16)]) == ['B', 'D', 'F', 'G']
+        assert sorted([f.id for f in cls.rec.surrounding_features(16, strand=1)]) == ['B', 'D']
+        assert sorted([f.id for f in cls.rec.surrounding_features(16, strand=-1)]) == ['F', 'G']
+        assert sorted([f.id for f in cls.rec.surrounding_features(15)]) == ['B', 'F', 'H']
+        assert sorted([f.id for f in cls.rec.surrounding_features(4)]) == ['A', 'B', 'F']
+        assert sorted([f.id for f in cls.rec.surrounding_features(26)]) == ['C', 'G']
+        assert sorted([f.id for f in cls.rec.surrounding_features(26, nearest=True)]) == ['G']
+        assert sorted([f.id for f in cls.rec.surrounding_features(16, nearest=True)]) == ['G']
+        assert sorted([f.id for f in cls.rec.surrounding_features(16, nearest=True, strand=1)]) == ['D']
+        assert sorted([f.id for f in cls.rec.surrounding_features(16, nearest=True, strand=-1)]) == ['G']
+        assert sorted([f.id for f in cls.rec.surrounding_features(11, nearest=True)]) == ['B', 'F']
+        assert sorted([f.id for f in cls.rec.surrounding_features(11, nearest=True, strand=1)]) == ['B']
+        assert sorted([f.id for f in cls.rec.surrounding_features(11, nearest=True, strand=-1)]) == ['F']
 
-class SeqRecord_StitchingTests(unittest.TestCase):
+class SeqRecordStitchingTests(unittest.TestCase):
     def test_stitch(self):
         assert True is False
+
+
+class SeqRecordComparisonTests(unittest.TestCase):
+    rec1 = SeqRecordEM2(SeqEM2.dna('ATGAGTCGGTAACGATGCATGCATGCAGCTGACGC'), id='Rec1', name='DummyDNA')
+    rec2 = SeqRecordEM2(SeqEM2.dna('ATGAGTCGGTAACGATGCATGCATGCAGCTGACGC'), id='Rec2', name='DummyDNA')
+    rec3 = SeqRecordEM2(SeqEM2.dna('ATGAGTCGGTAACGATGCATGCATGCAGCTGACGC'), id='Rec3', name='DummyDNA')
+
+    @classmethod
+    def test_comparisons(cls):
+        assert cls.rec1.__lt__(cls.rec2)
+        assert not cls.rec3.__lt__(cls.rec2)
+        assert cls.rec1.__le__(cls.rec2)
+        assert cls.rec1.__le__(cls.rec1)
+        assert not cls.rec2.__le__(cls.rec1)
+        assert cls.rec1.__eq__(cls.rec1)
+        assert not cls.rec3.__eq__(cls.rec2)
+        assert cls.rec1.__ne__(cls.rec2)
+        assert not cls.rec3.__ne__(cls.rec3)
+        assert cls.rec2.__gt__(cls.rec1)
+        assert not cls.rec1.__gt__(cls.rec2)
+        assert cls.rec3.__ge__(cls.rec2)
+        assert cls.rec1.__ge__(cls.rec1)
+        assert not cls.rec1.__ge__(cls.rec2)
+        assert cls.rec3 >= cls.rec3 > cls.rec1 == cls.rec1 < cls.rec2 <= cls.rec2
+        assert sorted([cls.rec3, cls.rec1, cls.rec2]) == [cls.rec1, cls.rec2, cls.rec3]
