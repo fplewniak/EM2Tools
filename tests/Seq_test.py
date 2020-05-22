068d9c33a14a3f5e834fc1ee097951e07a3e8df7
fplewniak <f.plewniak@unistra.fr>
2020-05-22 14:31:47 +0200

    Changed tests to pytest using fixtures defined in conftest.py

--- tests/Seq_test.py
+++ tests/Seq_test.py
@@ -1,40 +1,26 @@
-import unittest
-from EM2Libs.Seq import SeqEM2
-import pytest
+def test_is_protein(protein_sequence, dna_sequence):
+    assert protein_sequence.is_protein() == True
+    assert dna_sequence.is_protein() == False
 
 
-@pytest.fixture(scope="class")
-def seq_data(request):
-    request.cls.prot = SeqEM2.protein('HITHEREFREDANDGREG')
-    request.cls.dna = SeqEM2.dna('CATAGCGCGCGGCTAAA')
+def test_re_search(protein_sequence, dna_sequence):
+    assert {m.group(): m.start() for m in dna_sequence.re_search(r'TAG')} == {'TAG': 2}
+    assert {m.group(): m.start() for m in dna_sequence.re_search(r'CG{2}')} == {'CGG': 9}
+    assert {m.group(): m.start() for m in dna_sequence.re_search(r'(GC){2,3}')} == {'GCGCGC': 4}
+    assert {m.group(): m.start() for m in dna_sequence.re_search(r'[GC]{2,10}')} == {'GCGCGCGGC': 4}
+    assert {m.group(): m.start() for m in dna_sequence.re_search(r'[^GC]{2,}')} == {'ATA': 1, 'TAAA': 13}
+    assert {m.group(): m.start() for m in protein_sequence.re_search(r'THERE')} == {'THERE': 2}
+    assert {m.group(): m.start() for m in protein_sequence.re_search(r'(RE[FD]){2,3}')} == {'REFRED': 5}
+    assert {m.group(): m.start() for m in protein_sequence.re_search(r'[^GC]{2,}')} == {'HITHEREFREDAND': 0, 'RE': 15}
 
 
-@pytest.mark.usefixtures("seq_data")
-class SeqEM2Test(unittest.TestCase):
-    def test_is_protein(self):
-        assert self.prot.is_protein() == True
-        assert self.dna.is_protein() == False
-
-
-@pytest.mark.usefixtures("seq_data")
-class SeqSearch(unittest.TestCase):
-    def test_re_search(self):
-        assert {m.group(): m.start() for m in self.dna.re_search(r'TAG')} == {'TAG': 2}
-        assert {m.group(): m.start() for m in self.dna.re_search(r'CG{2}')} == {'CGG': 9}
-        assert {m.group(): m.start() for m in self.dna.re_search(r'(GC){2,3}')} == {'GCGCGC': 4}
-        assert {m.group(): m.start() for m in self.dna.re_search(r'[GC]{2,10}')} == {'GCGCGCGGC': 4}
-        assert {m.group(): m.start() for m in self.dna.re_search(r'[^GC]{2,}')} == {'ATA': 1, 'TAAA': 13}
-        assert {m.group(): m.start() for m in self.prot.re_search(r'THERE')} == {'THERE': 2}
-        assert {m.group(): m.start() for m in self.prot.re_search(r'(RE[FD]){2,3}')} == {'REFRED': 5}
-        assert {m.group(): m.start() for m in self.prot.re_search(r'[^GC]{2,}')} == {'HITHEREFREDAND': 0, 'RE': 15}
-
-    def test_search(self):
-        assert {m.group(): m.start() for m in self.dna.search('TAG')} == {'TAG': 2}
-        assert {m.group(): m.start() for m in self.dna.search('CG(2)')} == {'CGG': 9}
-        assert {m.group(): m.start() for m in self.dna.search('(GC)(2,3)')} == {'GCGCGC': 4}
-        assert {m.group(): m.start() for m in self.dna.search('[GC](2,10)')} == {'GCGCGCGGC': 4}
-        assert {m.group(): m.start() for m in self.dna.search('{GC}(2,)')} == {'ATA': 1, 'TAAA': 13}
-        assert {m.group(): m.start() for m in self.prot.search('THERE')} == {'THERE': 2}
-        assert {m.group(): m.start() for m in self.prot.search('(RE[FD])(2,3)')} == {'REFRED': 5}
-        assert {m.group(): m.start() for m in self.prot.search('{GC}(2,)')} == {'HITHEREFREDAND': 0, 'RE': 15}
-        assert self.prot.search('RRRRRRR') == []
+def test_search(protein_sequence, dna_sequence):
+    assert {m.group(): m.start() for m in dna_sequence.search('TAG')} == {'TAG': 2}
+    assert {m.group(): m.start() for m in dna_sequence.search('CG(2)')} == {'CGG': 9}
+    assert {m.group(): m.start() for m in dna_sequence.search('(GC)(2,3)')} == {'GCGCGC': 4}
+    assert {m.group(): m.start() for m in dna_sequence.search('[GC](2,10)')} == {'GCGCGCGGC': 4}
+    assert {m.group(): m.start() for m in dna_sequence.search('{GC}(2,)')} == {'ATA': 1, 'TAAA': 13}
+    assert {m.group(): m.start() for m in protein_sequence.search('THERE')} == {'THERE': 2}
+    assert {m.group(): m.start() for m in protein_sequence.search('(RE[FD])(2,3)')} == {'REFRED': 5}
+    assert {m.group(): m.start() for m in protein_sequence.search('{GC}(2,)')} == {'HITHEREFREDAND': 0, 'RE': 15}
+    assert protein_sequence.search('RRRRRRR') == []
