c75df79c7d8e31e3ed77f040c4659fdb60f8b181
fplewniak <f.plewniak@unistra.fr>
2020-10-02 16:07:59 +0200

    MappingProfile.get_values and get_references_with_max
    get_values returns a list of all values in profiles that may be used directly for statistics.
    get_references_with_max returns the maximum value in profile and a list of references whose profile reaches this value.

--- src/em2lib/ngs.py
+++ src/em2lib/ngs.py
@@ -79,6 +79,23 @@ def position(ref, start, end, bam):
     return p
 
 
+def mapping_qualities(ref, start, end, bam):
+    """
+    Returns all the mapping quality values in the specified reference region
+
+    :param ref: reference sequence in bam file
+    :param start: 0-based start of region to compute the profile of
+    :param end: 0-based end of region to compute the profile of (this position is included in the profile)
+    :param bam: pysam AlignmentFile handler
+    :return: a list of all the mapping quality values
+    """
+    qval = []
+    for col in bam.pileup(ref, start, end):
+        if start <= col.reference_pos <= end:
+            qval += col.get_mapping_qualities()
+    return qval
+
+
 class MappingProfile:
     """
     Class for computing mapping quality or coverage profiles along reference sequences from mapped reads in a BAM file.
@@ -114,3 +131,42 @@ class MappingProfile:
                 stop = ref_length if end is None else min(end, ref_length)
                 profiles[ref] = profile(ref, start, stop, self.bam)
         return profiles
+
+    def get_values(self, profile=mapping_qualities, references=None, start=0, end=None):
+        """
+        Method to get all values of some mapping statistics in all references and regions pooled into one single array.
+
+        :param profile: the function responsible for computing profile values, should take a reference name, start and
+         end positions, and the bam file handler, must return a profile
+        :param references: a reference name or a list thereof
+        :param start: start position for each reference sequence
+        :param end: end position for each reference, if None, the whole reference sequence is used
+        :return: an array with all values
+        """
+        profiles = self.get_profiles(profile=profile, references=references, start=start, end=end)
+        mapping_values = []
+        for ref in profiles:
+            mapping_values += profiles[ref]
+        return mapping_values
+
+    def get_references_with_max(self, profile=number_of_reads, references=None, start=0, end=None):
+        """
+        Returns the references whose profile contains the maximum value across profiles of all references
+
+        :param profile: the function responsible for computing profile values, should take a reference name, start and
+         end positions, and the bam file handler, must return a profile
+        :param references: a reference name or a list thereof
+        :param start: start position for each reference sequence
+        :param end: end position for each reference, if None, the whole reference sequence is used
+        :return: a tuple containing the maximum value and a list of all references reaching that value
+        """
+        profiles = self.get_profiles(profile=profile, references=references, start=start, end=end)
+        max_value = 0
+        ref_max = []
+        for ref in profiles:
+            if max(profiles[ref]) > max_value:
+                max_value = max(profiles[ref])
+                ref_max = [ref]
+            elif max(profiles[ref]) == max_value:
+                ref_max.append(ref)
+        return max_value, ref_max
